5f8dac97b5e4d620ae958bf9100e772c13a7b9c3
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-26 20:55:03 +0530

    create singelton desin pattern

--- src/com/developersstack/edumanage/db/DbConnection.java
+++ src/com/developersstack/edumanage/db/DbConnection.java
@@ -1,5 +1,20 @@
 package com.developersstack.edumanage.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
-    //impl
+    public static DbConnection dbConnection;
+    private Connection connection;
+    private DbConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/edu_manage", "root", "1234");
+    }
+    public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return null==dbConnection? (dbConnection=new DbConnection()):dbConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
 }
