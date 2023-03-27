d80383271e6a41e286939e5ef505d9e5ee643776
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-27 12:53:01 +0530

    implements saveStudent method in dataAccsesscode

--- src/com/developersstack/edumanage/db/DatabaseAccsessCode.java
+++ src/com/developersstack/edumanage/db/DatabaseAccsessCode.java
@@ -1,12 +1,10 @@
 package com.developersstack.edumanage.db;
 
+import com.developersstack.edumanage.model.Student;
 import com.developersstack.edumanage.model.User;
 import com.developersstack.edumanage.util.security.PasswordManager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseAccsessCode {
     public boolean saveUSer(User user) throws SQLException, ClassNotFoundException {
@@ -16,6 +14,16 @@ public class DatabaseAccsessCode {
         pstm.setString(2, user.getFirstName());
         pstm.setString(3, user.getLastName());
         pstm.setString(4,user.getPassword());
+        return pstm.executeUpdate()>0;
+    }
+
+    public boolean saveStudent(Student student) throws SQLException, ClassNotFoundException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO student VALUES (?,?,?,?)");
+        pstm.setString(1,student.getStudentId());
+        pstm.setString(2,student.getFullName());
+        pstm.setDate(3, (Date) student.getDateOfBirth());
+        pstm.setString(4,student.getAddress());
         return pstm.executeUpdate()>0;
     }
 
