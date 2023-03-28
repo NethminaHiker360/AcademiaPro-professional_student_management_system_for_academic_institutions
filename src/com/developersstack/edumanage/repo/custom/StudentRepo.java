b988480c8f2602ea5a1adad7206ede05cb8b5010
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-28 18:40:23 +0530

    overide method saveStudent

--- src/com/developersstack/edumanage/repo/custom/StudentRepo.java
+++ src/com/developersstack/edumanage/repo/custom/StudentRepo.java
@@ -2,10 +2,11 @@ package com.developersstack.edumanage.repo.custom;
 
 import com.developersstack.edumanage.entity.Student;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface StudentRepo {
-    public boolean saveStudent(Student student);
+    public boolean saveStudent(Student student) throws SQLException, ClassNotFoundException;
     public String findStudentLastId();
     public Student findStudent(String studentId);
     public boolean updateStudent(Student student);
--- src/com/developersstack/edumanage/repo/custom/impl/StudentRepoImpl.java
+++ src/com/developersstack/edumanage/repo/custom/impl/StudentRepoImpl.java
@@ -1,14 +1,24 @@
 package com.developersstack.edumanage.repo.custom.impl;
 
+import com.developersstack.edumanage.db.DbConnection;
 import com.developersstack.edumanage.entity.Student;
 import com.developersstack.edumanage.repo.custom.StudentRepo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class StudentRepoImpl implements StudentRepo {
     @Override
-    public boolean saveStudent(Student student) {
-        return false;
+    public boolean saveStudent(Student student) throws SQLException, ClassNotFoundException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO student VALUES (?,?,?,?)");
+        pstm.setString(1,student.getStudentId());
+        pstm.setString(2,student.getFullName());
+        pstm.setObject(3,student.getDateOfBirth());
+        pstm.setString(4,student.getAddress());
+        return pstm.executeUpdate()>0;
     }
 
     @Override
