c5880633e353bd39df8185e7eb49cdc45d73f12f
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-01 12:40:25 +0530

    add crudRepo

---
+++ src/com/developersstack/edumanage/repo/CrudRepo.java
@@ -0,0 +1,11 @@
+package com.developersstack.edumanage.repo;
+
+import java.util.ArrayList;
+
+public interface CrudRepo<T, ID> {
+    public boolean save(T t);
+    public boolean update(T t);
+    public boolean delete(T t);
+    public T find(ID id);
+    public ArrayList<T> findAll();
+}
