1e42743b9a9a139c2e80e478091b5398c1b7bdbf
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-01 11:44:33 +0530

    implement CrudUtil

--- src/com/developersstack/edumanage/repo/custom/CrudUtil.java
+++ src/com/developersstack/edumanage/repo/custom/CrudUtil.java
@@ -1,4 +1,20 @@
 package com.developersstack.edumanage.repo.custom;
 
+import com.developersstack.edumanage.db.DbConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CrudUtil {
+    public static <T> T execute(String sql, Object... params) throws SQLException, ClassNotFoundException {
+        PreparedStatement pstm = DbConnection.getInstance().getConnection().prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            pstm.setObject((i+1),params[i]);
+        }
+        if (sql.startsWith("SELECT")){
+            return (T) pstm.executeQuery();
+        }else {
+            return (T) (Boolean) (pstm.executeUpdate()>0);
+        }
+    }
 }
