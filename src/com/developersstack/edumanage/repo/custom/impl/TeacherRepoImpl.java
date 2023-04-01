499c4e07134a97f0a987202928b0303e0bd085a0
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-01 12:01:52 +0530

    implement CrudUtil with TeacherRepoImpl

--- src/com/developersstack/edumanage/repo/custom/impl/TeacherRepoImpl.java
+++ src/com/developersstack/edumanage/repo/custom/impl/TeacherRepoImpl.java
@@ -2,6 +2,7 @@ package com.developersstack.edumanage.repo.custom.impl;
 
 import com.developersstack.edumanage.db.DbConnection;
 import com.developersstack.edumanage.entity.Teacher;
+import com.developersstack.edumanage.repo.CrudUtil;
 import com.developersstack.edumanage.repo.custom.TeacherRepo;
 
 import java.sql.Connection;
@@ -13,13 +14,8 @@ import java.util.ArrayList;
 public class TeacherRepoImpl implements TeacherRepo {
     @Override
     public boolean saveTeacher(Teacher teacher) throws SQLException, ClassNotFoundException {
-        Connection connection = DbConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO teacher VALUES (?,?,?,?)");
-        pstm.setString(1,teacher.getCode());
-        pstm.setString(2,teacher.getName());
-        pstm.setString(3,teacher.getAddress());
-        pstm.setString(4,teacher.getCode());
-        return pstm.executeUpdate()>0;
+        return CrudUtil.execute("INSERT INTO teacher VALUES (?,?,?,?)",teacher.getCode(),teacher.getName()
+                ,teacher.getAddress(),teacher.getCode());
     }
 
     @Override
