8bc76d173c0d64f714a5c1a5b852a7196d7e7db3
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-02 14:50:27 +0530

    create UserRepo

--- src/com/developersstack/edumanage/repo/custom/UserRepo.java
+++ src/com/developersstack/edumanage/repo/custom/UserRepo.java
@@ -1,9 +1,12 @@
 package com.developersstack.edumanage.repo.custom;
 
+import com.developersstack.edumanage.dto.UserDto;
 import com.developersstack.edumanage.entity.User;
 import com.developersstack.edumanage.repo.CrudRepo;
 
 import java.sql.SQLException;
 
-public interface UserRepo extends CrudRepo<User,String> {
+public interface UserRepo extends CrudRepo<UserDto,String> {
+    public boolean saveUser(UserDto dto);
+    public UserDto findUser(String id);
 }
