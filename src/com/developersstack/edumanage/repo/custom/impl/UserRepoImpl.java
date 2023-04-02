c3e42bf94e208bcdb30e7e86fa92027b07a5f09c
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-02 13:06:02 +0530

    apply facade design pattern to UserRepo

--- src/com/developersstack/edumanage/repo/custom/impl/UserRepoImpl.java
+++ src/com/developersstack/edumanage/repo/custom/impl/UserRepoImpl.java
@@ -6,6 +6,7 @@ import com.developersstack.edumanage.repo.custom.UserRepo;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UserRepoImpl implements UserRepo {
     @Override
@@ -21,6 +22,31 @@ public class UserRepoImpl implements UserRepo {
             return new User(rst.getString(2),rst.getString(3),
                     rst.getString(1),rst.getString(4));
         }
+        return null;
+    }
+
+    @Override
+    public boolean save(User user) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(User user) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public User find(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<User> findAll() {
         return null;
     }
 }
