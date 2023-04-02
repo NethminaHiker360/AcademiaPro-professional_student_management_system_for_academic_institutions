eedd7ca194bde6e916e25560742f24b9299896b4
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-02 13:34:31 +0530

    create dto package

--- src/com/developersstack/edumanage/bo/custom/StudentBo.java
+++ src/com/developersstack/edumanage/bo/custom/StudentBo.java
@@ -1,13 +1,15 @@
 package com.developersstack.edumanage.bo.custom;
 
+import com.developersstack.edumanage.dto.StudentDto;
+
 import java.util.ArrayList;
 
 public interface StudentBo {
-    public boolean save(StudentDto dto);
-    public boolean update(StudentDto dto);
-    public boolean delete(String id);
-    public StudentDto find(String id);
-    public ArrayList<StudentDto> findAll();
+    public boolean saveStudent(StudentDto dto);
+    public boolean updateStudent(StudentDto dto);
+    public boolean deleteStudent(String id);
+    public StudentDto findStudent(String id);
+    public ArrayList<StudentDto> findAllStudents();
     public String findStudentLastId();
-    public ArrayList<StudentDto> findAllStudents(String searchText);
+    public ArrayList<StudentDto> searchStudents(String searchText);
 }
