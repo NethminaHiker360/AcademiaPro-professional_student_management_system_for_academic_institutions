1dea4379a26c13a714c0d801d69d1049158c92fd
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-04-02 13:27:01 +0530

    add methods to StudentBo

--- src/com/developersstack/edumanage/bo/custom/StudentBo.java
+++ src/com/developersstack/edumanage/bo/custom/StudentBo.java
@@ -4,4 +4,10 @@ import java.util.ArrayList;
 
 public interface StudentBo {
     public boolean save(StudentDto dto);
+    public boolean update(StudentDto dto);
+    public boolean delete(String id);
+    public StudentDto find(String id);
+    public ArrayList<StudentDto> findAll();
+    public String findStudentLastId();
+    public ArrayList<StudentDto> findAllStudents(String searchText);
 }
